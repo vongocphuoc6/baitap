420f90092504a9e3e62eae879b8998fdbe6640d7
phuoc1314 <105440354+WYf460@users.noreply.github.com>
2024-11-12 20:53:30 +0700

    Update huy_session.php

--- bai10/huy_session.php
+++ bai10/huy_session.php
@@ -1,28 +1,12 @@
 sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository universe
+sudo apt install -y ros-humble-turtlebot3* 
 
-sudo apt update && sudo apt install -y curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo gpg --dearmor -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-sudo apt update
-sudo apt install -y ros-humble-desktop
-
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt install -y python3-argcomplete
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
 
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" > /etc/apt/sources.list.d/gazebo-stable.list'
-wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+ros2 launch turtlebot3_gazebo turtlebot3_house.launch.py
 
-sudo apt update
-sudo apt install -y gazebo11 libgazebo11-dev
+ros2 run turtlebot3_teleop teleop_keyboard
 
-sudo apt install -y ros-humble-gazebo-ros-pkgs
-
-gazebo
-
-ros2 launch gazebo_ros gazebo.launch.py
