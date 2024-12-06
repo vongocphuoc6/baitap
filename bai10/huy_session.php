23bd557d2dcffef71a7c6237141f9bb299268983
phuoc1314 <105440354+WYf460@users.noreply.github.com>
2024-12-06 19:25:52 +0700

    Update huy_session.php

--- bai10/huy_session.php
+++ bai10/huy_session.php
@@ -9,4 +9,4 @@ ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
 ros2 launch turtlebot3_gazebo turtlebot3_house.launch.py
 
 ros2 run turtlebot3_teleop teleop_keyboard
-
+echo $TURTLEBOT3_MODEL
