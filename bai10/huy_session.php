8df3ce703f9cb96b563f1273d8bc95be77134f7a
phuoc1314 <105440354+WYf460@users.noreply.github.com>
2024-12-07 20:51:15 +0700

    Update huy_session.php

--- bai10/huy_session.php
+++ bai10/huy_session.php
@@ -88,3 +88,35 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+// tt
+cd ~/ros2_ws/src/turtlebot3_keyboard_control
+nano setup.py
+
+from setuptools import setup
+
+package_name = 'turtlebot3_keyboard_control'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Node điều khiển TurtleBot 3 bằng bàn phím.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'keyboard_control = turtlebot3_keyboard_control.keyboard_control:main',
+        ],
+    },
+)
+
