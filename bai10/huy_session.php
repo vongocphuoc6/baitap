be3ee1b4306e43a60831ea12396da573521a3095
phuoc1314 <105440354+WYf460@users.noreply.github.com>
2024-12-07 21:49:17 +0700

    Update huy_session.php

--- bai10/huy_session.php
+++ bai10/huy_session.php
@@ -138,3 +138,30 @@ sudo apt install python3-colcon-common-extensions
 colcon --help
 source ~/ros2_ws/install/setup.bash
 ros2 pkg list | grep turtlebot3_keyboard_control
+
+from setuptools import find_packages, setup
+
+package_name = 'turtlebot3_keyboard_control'
+
+setup(
+ name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='phuoc',
+    maintainer_email='phuoc@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'keyboard_control = turtlebot3_keyboard_control.keyboard_control:ma>
+        ],
+    },
+)
